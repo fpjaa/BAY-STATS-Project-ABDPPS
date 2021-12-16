88583ca0586174d0496ca068527ef6b636dd7fe0
CasparDietz <35566632+CasparDietz@users.noreply.github.com>
2021-12-16 12:16:10 +0100

    Started working on MC_Sample_K but need some help for the theory

--- rgwish.R
+++ rgwish.R
@@ -1,21 +1,27 @@
-#Working directory setup
-print(getwd())
-setwd("/Users/caspar/Desktop/Bayesian Project/BAY-STATS-Project-ABDPPS")
+#Use BDgrapg
+library(BDgraph)
 
-#Read the adjacency from the input csv file (adj.csv)
+#Read the b that is parsed as an argument from Python
+parser = commandArgs(trailingOnly=TRUE)
+b = parser[1]
+#Read the n (number of samples required) that is parsed from Python
+n = parser[2]
+
+#Read the adjacency matrix from the input csv file (adj.csv)
 adj <- read.csv("adj.csv", header=FALSE)
 adj = data.matrix(adj)
 
-#Calculate b=k-1
-dim = dim(adj)
-b = dim[1]-1
+#Read the shape_atrix from the input csv file (shape.csv)
+shape <- read.csv("shape.csv", header=FALSE)
+shape = data.matrix(shape)
 
 #Call rgwish
-sample <- rgwish( n = 1, adj = adj, b = b, D = diag( b ) )
+sprintf("[R] rgwish will now be sampling %s samples. The b (degrees of freedom) is set to %s", n,b)
+sample <- rgwish( n = n, adj = adj, b = NULL, D = NULL )
 #Round to two decimals
 output <- round( sample, 2 )
 
 #Write the result in the output file (gwish.csv)
-write.table(output, file = "gwish.csv", row.names = FALSE, col.names = FALSE)
+write.table(output, file = "gwish.csv", sep = ",", row.names = FALSE, col.names = FALSE)
 
-
+print("[R] rgwish succesfully executed!")
