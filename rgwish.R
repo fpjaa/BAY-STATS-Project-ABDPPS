f028c3dd148288baa7d473471348f33cac0ae0fd
Kanthavel <kanthavel@kanthavel-Latitude-5501>
2022-01-04 16:53:25 +0100

    Update rgwish.R

--- rgwish.R
+++ rgwish.R
@@ -6,6 +6,9 @@ parser = commandArgs(trailingOnly=TRUE)
 b = parser[1]
 #Read the n (number of samples required) that is parsed from Python
 n = parser[2]
+#Read the if debugging lines should be printed
+debugOn = parser[3]
+
 
 #Read the adjacency matrix from the input csv file (adj.csv)
 adj <- read.csv("adj.csv", header=FALSE)
@@ -17,11 +20,15 @@ shape <- as.matrix(shape)
 rownames(shape) <- colnames(shape)<-NULL
 
 #Call rgwish
-sprintf("[R] rgwish will now be sampling %s samples. The b (degrees of freedom) is set to %s", n,b)
+if (debugOn){
+    sprintf("[R] rgwish will now be sampling %s samples. The b (degrees of freedom) is set to %s", n,b)
+}
 sample <- rgwish( n = n, adj = adj, b = b, D = shape )
 #Round to two decimals
 output <- round( sample, 2 )
 
 #Write the result in the output file (gwish.csv)
 write.table(output, file = "gwish.csv", sep = ",", row.names = FALSE, col.names = FALSE)
-print("[R] rgwish succesfully executed!")
+if (debugOn){
+    print("[R] rgwish succesfully executed!")
+}
