22697637acfc5d2db7f009d3b8350dbfa028040a
CasparDietz <35566632+CasparDietz@users.noreply.github.com>
2022-01-16 21:40:12 +0100

    Added R script for BDMCMC sampling

--- bdmcmc.R
+++ bdmcmc.R
@@ -52,7 +52,7 @@ if (debugOn){
 sample.bdmcmc <- bdgraph( K, n = NULL, method = "ggm", algorithm = "bdmcmc", iter = 1, burnin = 0, not.cont = NULL, g.prior = p_existing_link, df.prior = b, g.start = "empty", jump = 1, save = TRUE, print = 1000, cores = "all", threshold = 1e-8 )
 G = summary(sample.bdmcmc)$p_links
 #G = summary(sample.bdmcmc)['p_links']
-K = summary(sample.bdmcmc)$K_hat$
+K = summary(sample.bdmcmc)$K_hat
 #K = summary(sample.bdmcmc)['K_hat']
 #HOW CAN WE KEEP THE ENTIRE BDGRAPH OBJECT FROM ONE ITERATION TO THE OTHER
 
