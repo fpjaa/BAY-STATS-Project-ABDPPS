0bcf04ff64fc38a106b8a325273efe58c55a8e9a
Kanthavel <kanthavel@kanthavel-Latitude-5501>
2022-01-17 19:34:07 +0100

    Update bdmcmc.R

--- bdmcmc.R
+++ bdmcmc.R
@@ -13,20 +13,16 @@ n = as.integer(parser[2])
 G = read.table(text=parser[3])  # Input is already in the right format thanks to python
 G = as.matrix(G)
 
-# Reading K matrix
-K = read.table(text=parser[4])  # Input is already in the right format thanks to python
-K = as.matrix(K)
-
 # Reading Data matrix (that is, the k x k matrix built from multivariate Gaussian samples)
 # Important: Data is already H' . H
-Data = read.table(text=parser[5])  # Input is already in the right format thanks to python
+Data = read.table(text=parser[4])  # Input is already in the right format thanks to python
 Data = as.matrix(Data)
 
 #Read the if debugging lines should be printed
-debugOn = parser[6]
+debugOn = parser[5]
 
 # Building shape_matrix
-shape_matrix = diag(dim(K)[1])  # Identity matrix of size k
+shape_matrix = diag(dim(G)[1])  # Identity matrix of size k
 
 # Prior on existing link
 p_existing_link = 0.5  # uninformative prior
@@ -35,11 +31,9 @@ if (debugOn){
     sprintf("[R] BDMCMC will now be sampling a single iteration.")
     sprintf("The n (data sample size) is set to %d.", n)
     sprintf("The b (degrees of freedom) is set to %d", b)
-    sprintf("The proobability of existing link is set to %f", p_existing_link)
+    sprintf("The probability of existing link is set to %f", p_existing_link)
     print("[R] Matrix G:")
     print(G)
-    print("[R] Matrix K:")
-    print(K)
     print("[R] Data Matrix:")
     print(Data)
     print("[R] Shape Matrix:")
@@ -49,11 +43,10 @@ if (debugOn){
 # INPUTS: b, n, G, K, Data, p_existing_link and shape_matrix (if you need something else let me know - K)
 #########################################
 # Actual algorithm
-sample.bdmcmc <- bdgraph( Data, n = NULL, method = "ggm", algorithm = "bdmcmc", iter = 1, burnin = 0, not.cont = NULL, g.prior = p_existing_link, df.prior = b, g.start = G, jump = 1, save = TRUE, print = 1000, cores = "all", threshold = 1e-8 )
-G = summary(sample.bdmcmc)$p_links
-#G = summary(sample.bdmcmc)['p_links']
-K = summary(sample.bdmcmc)$K_hat
-#K = summary(sample.bdmcmc)['K_hat']
+sample.bdmcmc <- bdgraph(Data, n = n, method = "ggm", algorithm = "bdmcmc", iter = 1, burnin = 0, not.cont = NULL, g.prior = p_existing_link, df.prior = b, g.start = G, jump = 1, save = TRUE, print = 1000, cores = "all", threshold = 1e-8 )
+G = summary(sample.bdmcmc)$selected_g
+K = sample.bdmcmc$K_hat
+waitingTime = summary(sample.bdmcmc)$graph_weights[[0]]
 #HOW CAN WE KEEP THE ENTIRE BDGRAPH OBJECT FROM ONE ITERATION TO THE OTHER
 
 # Check https://github.com/TeoGiane/FGM/blob/master/R/sampler.R
