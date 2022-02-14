abea659aae505f9e38d8b929871fba7beb489ccb
Kanthavel <kanthavel@kanthavel-Latitude-5501>
2022-02-14 23:39:36 +0100

    Update 04_Main_Cycle_FINAL.ipynb and graph_loss.py

--- graph_loss.py
+++ graph_loss.py
@@ -3,32 +3,37 @@ from sklearn.datasets import make_sparse_spd_matrix
 from scipy.spatial import distance
 
 def permutation_topics(B_true, B_sampled):
-    B_urn=B_sampled.copy()
-    k,V=B_true.shape
-    B_urn[:,k+1]=range(k)
-    permutation=[]
+    B_urn = B_sampled.copy()
+    k, V = B_true.shape
+    B_urn[:, k+1] = range(k)
+    permutation = []
     for i in range(k):
-        distances=[]
+        distances = []
         for j in range(k-i):
-            distances.append(np.linalg.norm(B_true[i]-B_urn[j]))
+            distances.append(np.linalg.norm(B_true[i] - B_urn[j]))
         min_index = np.argmin(distances)
-        permutation.append(B_urn[min_index,k+1])
-        B_urn=np.delete(B_urn,min_index,0)
+        permutation.append(B_urn[min_index, k+1])
+        B_urn = np.delete(B_urn, min_index, 0)
     return permutation
-    
 
-def graph_loss(G_true,G_sampled,permutation):
-    G_perm=G_sampled.copy()
-    print("BEFORE FOR")
-    for i in range(int(len(G_true[0]))):
-        p=int(permutation[i])
-        G_perm[i]=G_sampled[p]
-        print("G_PERM")
-        print(type(G_perm))
-        G_perm[:,i]=G_sampled[:,p]
-        print("G_SAMPLED")
-        print(type(G_sampled))
-        print("NEW RETURN")
-        print(abs(G_true-G_perm).sum())
+
+def _fake_log(*args, **kwargs):
+    return
+
+
+def graph_loss(G_true, G_sampled, permutation, debug=False):
+    log = print if debug else _fake_log  # turn log off/on
+    G_perm = G_sampled.copy()
+    log("BEFORE FOR")
+    for i in range(G_true.shape[0]):  # Number of rows
+        p = int(permutation[i])
+        G_perm[i] = G_sampled[p]
+        log("G_PERM")
+        log(G_perm)
+        G_perm[:,i] = G_sampled[:,p]
+        log("G_SAMPLED")
+        log(G_sampled)
+        log("NEW RETURN")
+        log(abs(G_true-G_perm).sum())
     return abs(G_true-G_perm).sum()
 
